033c9fec823700079db6b631be74b699eac156a1
Aryan Zutshi <122102323+Aryanzutshi@users.noreply.github.com>
2025-04-03 17:49:19 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,10 +6,10 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
           alt="Next.js logo"
-          width={180}
+          src="https://nextjs.org/icons/next.svg"
           height={38}
+          width={180}
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
@@ -26,14 +26,14 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             rel="noopener noreferrer"
           >
             <Image
               className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
               alt="Vercel logomark"
+              src="https://nextjs.org/icons/vercel.svg"
               width={20}
               height={20}
             />
